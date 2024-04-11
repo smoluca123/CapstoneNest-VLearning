604a211f348c95d1f246c8a691771cf43ece313d
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-04-11 17:14:41 +0700

    update db file 11/4/2024

--- db_export/elearning.sql
+++ db_export/elearning.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50739 (5.7.39-log)
  File Encoding         : 65001
 
- Date: 09/04/2024 18:40:33
+ Date: 11/04/2024 17:14:08
 */
 
 SET NAMES utf8mb4;
@@ -67,13 +67,14 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_type_user`(`type`) USING BTREE,
   CONSTRAINT `user_type_user` FOREIGN KEY (`type`) REFERENCES `type_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'test1@gmail.com', 'test1', '$2b$10$4UP5n9oemGAUu07EOGfomutgAF5hnTCj4TSZNvF0XOiGnLvCSmELW', 'Test 1', '0000000000', 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0MSIsImtleSI6MTcxMjY2Mjc0NjIwOCwiaWF0IjoxNzEyNjYyNzQ2LCJleHAiOjE3MTUyNTQ3NDZ9.Pyp69DluPx_7Ku3STz1ADo1DslDJ_OHdeCJaz25xBBY', 0, 0);
-INSERT INTO `user` VALUES (7, 'dev1@gmail.com', 'dev1', '$2b$10$KUe8Sv9bOK5n.P.85wBueu9aQb0j5cvmD7yvXRBK5/HJzBON0YJ0q', 'Dev 1', '0909090909', 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidXNlcm5hbWUiOiJkZXYxIiwia2V5IjoxNzEyMjU0NjI3OTg0LCJpYXQiOjE3MTIyNTQ2MjcsImV4cCI6MTcxNDg0NjYyN30.PgqbyOmje03_MCQCeVtDHruQqKKhLg_11wRHVuDUE0c', 1, 0);
-INSERT INTO `user` VALUES (8, 'test2@gmail.com', 'test2', '$2b$10$13YW8DtIqsdv4oq4C6XLoentGEX0N6yt0PdgyNohSpzoV18K14PGa', 'Test 2', '0909090909', 1, NULL, 1, 1);
+INSERT INTO `user` VALUES (1, 'test1@gmail.com', 'test1', '$2b$10$4UP5n9oemGAUu07EOGfomutgAF5hnTCj4TSZNvF0XOiGnLvCSmELW', 'Test 1', '0000000000', 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0MSIsImtleSI6MTcxMjgyNTQ4Mzc1MiwiaWF0IjoxNzEyODI1NDgzLCJleHAiOjE3MTU0MTc0ODN9.toYHj6g3Wu3G-eX4WQOYsLqRDkDqWw81CdIa7HmUlX4', 1, 0);
+INSERT INTO `user` VALUES (7, 'dev1@gmail.com', 'dev1', '$2b$10$KUe8Sv9bOK5n.P.85wBueu9aQb0j5cvmD7yvXRBK5/HJzBON0YJ0q', 'Dev 1', '0909090909', 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidXNlcm5hbWUiOiJkZXYxIiwia2V5IjoxNzEyNzQ2NzM0MzczLCJpYXQiOjE3MTI3NDY3MzQsImV4cCI6MTcxNTMzODczNH0.lDGnJ9IkddkvBUCGDiy4hNRC7Tdm3fdQFC6vILUApLU', 1, 0);
+INSERT INTO `user` VALUES (8, 'test2@gmail.com', 'test2', '$2b$10$13YW8DtIqsdv4oq4C6XLoentGEX0N6yt0PdgyNohSpzoV18K14PGa', 'Test 2', '0909090909', 1, NULL, 1, 0);
+INSERT INTO `user` VALUES (11, 'abc@gmail.com', 'abc', '$2b$10$5q6bikHHxwFDVm.PksAskuRf3ow9iV1Pp8F4o0MobGaxXAx9i7g2C', 'Abc', '000', 1, NULL, 1, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
